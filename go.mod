82e3ff73e0c96c9d1619bdaddd0d659c93121c4d
beihai0xff <beihai@wingsxdu.com>
2021-12-27 22:26:13 +0800

    add log

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	go.uber.org/zap v1.19.1
 	google.golang.org/grpc v1.41.0
 	google.golang.org/protobuf v1.27.1
 )
---
+++ pkg/log/type.go
@@ -0,0 +1,13 @@
+package log
+
+import "go.uber.org/zap/zapcore"
+
+// Levels zapcore level
+var Levels = map[string]zapcore.Level{
+	"":      zapcore.DebugLevel,
+	"debug": zapcore.DebugLevel,
+	"info":  zapcore.InfoLevel,
+	"warn":  zapcore.WarnLevel,
+	"error": zapcore.ErrorLevel,
+	"fatal": zapcore.FatalLevel,
+}
